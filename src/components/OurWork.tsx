01613309728c909d4263b2ca7f17eb53fdec18d7
Ljupcho.Taskov-FE11 <ljupchotaskov1@gmail.com>
2023-12-17 23:53:17 +0100

    sdasa

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -15,7 +15,7 @@ const OurWork = () => {
         <div className="col-12 mb-3">
           <p className="desc-text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
