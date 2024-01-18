d495bd912619575a00ec5145fca50501cfd31ce3
Ljupcho.Taskov-FE11 <ljupchotaskov1@gmail.com>
2024-01-18 17:57:41 +0100

    changing arrows

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,62 +1,3 @@
-// import { useState } from "react";
-// import { ProductsType } from "../types/types";
-// import Link from "next/link";
-// import ProductItem from "./ProductItem";
-
-// interface Props {
-//   products: ProductsType[];
-// }
-
-// const ImageCarousel: React.FC<Props> = ({ products }) => {
-//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-//   const nextImage = () => {
-//     setCurrentImageIndex((prevIndex) =>
-//       prevIndex < products.length - 1 ? prevIndex + 1 : 0
-//     );
-//   };
-
-//   const previousImage = () => {
-//     setCurrentImageIndex((prevIndex) =>
-//       prevIndex > 0 ? prevIndex - 1 : products.length - 1
-//     );
-//   };
-//   const currentProduct = products[currentImageIndex];
-//   return (
-//     <section
-//       style={{
-//         background:
-//           "var(--Pink-gradient, linear-gradient(33deg, #FFDBDB 0.05%, #FDFDFD 93.1%, #FDFDFD 110.6%, #8A8328 152.03%))",
-//       }}
-//     >
-//       <div className="container py-4">
-//         <div className="row">
-//           <Link href={`/product/${currentProduct.id}`}>
-//             <div className="col-12">
-//               <div className="image-carousel">
-//                 <img src={products[currentImageIndex].img} alt="Product" />
-//                 <p>{currentProduct.title}</p>
-//                 <p>{currentProduct.priceR}</p>
-//                 <p className="pHeart">
-//                   <img src="../../logo/ph_heart-straight-thin.png" alt="" />
-//                 </p>
-//               </div>
-//             </div>
-//           </Link>
-//         </div>
-//         <div className="row">
-//           <div className="col d-flex justify-content-center">
-//             <p className="arrow-sliders prev" onClick={previousImage}></p>
-//             <p className="arrow-sliders next" onClick={nextImage}></p>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default ImageCarousel;
-
 import { useState } from "react";
 import { ProductsType } from "../types/types";
 import ProductItem from "./ProductItem";
@@ -100,8 +41,14 @@ const ImageCarousel: React.FC<Props> = ({ products }) => {
         </div>
         <div className="row">
           <div className="col d-flex justify-content-center">
-            <p className="arrow-sliders prev" onClick={previousImage}></p>
-            <p className="arrow-sliders next" onClick={nextImage}></p>
+            <i
+              className="fa-solid fa-angle-left fa-2xl"
+              onClick={previousImage}
+            ></i>
+            <i
+              className="fa-solid fa-angle-right fa-2xl"
+              onClick={nextImage}
+            ></i>
           </div>
         </div>
       </div>
