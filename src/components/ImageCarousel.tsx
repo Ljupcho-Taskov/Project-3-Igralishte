69a5f8f134311b6be05e7bc03036461235b008db
Ljupcho.Taskov-FE11 <ljupchotaskov1@gmail.com>
2024-02-02 16:22:23 +0100

    sliding on mobile

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -8,21 +8,18 @@ interface Props {
 
 const ImageCarousel: React.FC<Props> = ({ products }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [slideDirection, setSlideDirection] = useState<"left" | "right">();
-  const touchStartXRef = useRef(0);
+  const touchStartXRef = useRef<number | null>(null);
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) =>
       prevIndex < products.length - 1 ? prevIndex + 1 : 0
     );
-    setSlideDirection("left");
   };
 
   const previousImage = () => {
     setCurrentImageIndex((prevIndex) =>
       prevIndex > 0 ? prevIndex - 1 : products.length - 1
     );
-    setSlideDirection("right");
   };
 
   const handleTouchStart = (e: React.TouchEvent) => {
@@ -30,6 +27,8 @@ const ImageCarousel: React.FC<Props> = ({ products }) => {
   };
 
   const handleTouchEnd = (e: React.TouchEvent) => {
+    if (touchStartXRef.current === null) return;
+
     const touchEndX = e.changedTouches[0].clientX;
     const deltaX = touchEndX - touchStartXRef.current;
 
@@ -38,21 +37,25 @@ const ImageCarousel: React.FC<Props> = ({ products }) => {
     } else if (deltaX < -50) {
       nextImage();
     }
+
+    touchStartXRef.current = null;
   };
 
   return (
     <section>
       <div className="container py-5">
         <div
-          className="row mb-2 image-carousel-container"
+          className="row mb-3"
           onTouchStart={handleTouchStart}
           onTouchEnd={handleTouchEnd}
         >
-          <div className={`col-12 image-carousel ${slideDirection}`}>
-            <ProductItem
-              index={currentImageIndex}
-              {...products[currentImageIndex]}
-            />
+          <div className="col-12">
+            <div className="image-carousel">
+              <ProductItem
+                index={currentImageIndex}
+                {...products[currentImageIndex]}
+              />
+            </div>
           </div>
         </div>
         <div className="row">
@@ -71,3 +74,77 @@ const ImageCarousel: React.FC<Props> = ({ products }) => {
 };
 
 export default ImageCarousel;
+
+// import { useState, useRef } from "react";
+// import { ProductsType } from "../types/types";
+// import ProductItem from "./ProductItem";
+
+// interface Props {
+//   products: ProductsType[];
+// }
+
+// const ImageCarousel: React.FC<Props> = ({ products }) => {
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+//   const [slideDirection, setSlideDirection] = useState<"left" | "right">();
+//   const touchStartXRef = useRef(0);
+
+//   const nextImage = () => {
+//     setCurrentImageIndex((prevIndex) =>
+//       prevIndex < products.length - 1 ? prevIndex + 1 : 0
+//     );
+//     setSlideDirection("left");
+//   };
+
+//   const previousImage = () => {
+//     setCurrentImageIndex((prevIndex) =>
+//       prevIndex > 0 ? prevIndex - 1 : products.length - 1
+//     );
+//     setSlideDirection("right");
+//   };
+
+//   const handleTouchStart = (e: React.TouchEvent) => {
+//     touchStartXRef.current = e.touches[0].clientX;
+//   };
+
+//   const handleTouchEnd = (e: React.TouchEvent) => {
+//     const touchEndX = e.changedTouches[0].clientX;
+//     const deltaX = touchEndX - touchStartXRef.current;
+
+//     if (deltaX > 50) {
+//       previousImage();
+//     } else if (deltaX < -50) {
+//       nextImage();
+//     }
+//   };
+
+//   return (
+//     <section>
+//       <div className="container py-5">
+//         <div
+//           className="row mb-2 image-carousel-container"
+//           onTouchStart={handleTouchStart}
+//           onTouchEnd={handleTouchEnd}
+//         >
+//           <div className={`col-12 image-carousel ${slideDirection}`}>
+//             <ProductItem
+//               index={currentImageIndex}
+//               {...products[currentImageIndex]}
+//             />
+//           </div>
+//         </div>
+//         <div className="row">
+//           <div className="col d-flex justify-content-around">
+//             <p className="pointer" onClick={previousImage}>
+//               <i className="fa-solid fa-angle-left fa-2xl"></i>
+//             </p>
+//             <p className="pointer" onClick={nextImage}>
+//               <i className="fa-solid fa-angle-right fa-2xl"></i>
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default ImageCarousel;
