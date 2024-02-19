437ab1af09c134d45667d547a8fa434d2b64c5de
Ljupcho.Taskov-FE11 <ljupchotaskov1@gmail.com>
2024-02-19 19:49:08 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
-//   serverless: {
-//     maxDuration: 10000,
-//   },
 // };
 
 // module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  serverless: {
+    maxDuration: 30000,
+  },
+};
+
+module.exports = nextConfig;
